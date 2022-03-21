1cec6467d2fd4d0e2cc5ac7804cdbfec769d3f03
Shelly Tansil <shellytansil93@gmail.com>
2022-03-21 20:26:44 +0700

    add tv model test

---
+++ test/data/models/tv_model_test.dart
@@ -0,0 +1,38 @@
+import 'package:ditonton/data/models/tv_model.dart';
+import 'package:ditonton/domain/entities/tv.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  final tTvModel = TvModel(
+    backdropPath: '/path.jpg',
+    firstAirDate: 'firstAirDate',
+    genreIds: [1, 2, 3],
+    id: 1,
+    name: 'name',
+    originalName: 'originalName',
+    overview: 'overview',
+    popularity: 1.0,
+    posterPath: '/path.jpg',
+    voteAverage: 1.0,
+    voteCount: 1,
+  );
+
+  final tTv = Tv(
+    backdropPath: '/path.jpg',
+    firstAirDate: 'firstAirDate',
+    genreIds: [1, 2, 3],
+    id: 1,
+    name: 'name',
+    originalName: 'originalName',
+    overview: 'overview',
+    popularity: 1.0,
+    posterPath: '/path.jpg',
+    voteAverage: 1.0,
+    voteCount: 1,
+  );
+
+  test('should be a subclass of Tv entity', () async {
+    final result = tTvModel.toEntity();
+    expect(result, tTv);
+  });
+}
