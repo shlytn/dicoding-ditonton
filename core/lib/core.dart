d0be9455b5ee9bcb1c871de8caf937e869bfa7af
Shelly Tansil <shellytansil93@gmail.com>
2022-03-21 22:59:26 +0700

    export code

--- core/lib/core.dart
+++ core/lib/core.dart
@@ -1,7 +1,8 @@
 library core;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'styles/colors.dart';
+export 'styles/text_styles.dart';
+export 'utils/constants.dart';
+export 'utils/exception.dart';
+export 'utils/failure.dart';
+export 'utils/state_enum.dart';
