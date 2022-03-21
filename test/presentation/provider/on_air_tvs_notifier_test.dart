35b6c1d477f46291ce6742a7f0b174282849a850
Shelly Tansil <shellytansil93@gmail.com>
2022-03-21 20:28:19 +0700

    add tv provider test

---
+++ test/presentation/provider/on_air_tvs_notifier_test.dart
@@ -0,0 +1,76 @@
+import 'package:dartz/dartz.dart';
+import 'package:ditonton/common/failure.dart';
+import 'package:ditonton/common/state_enum.dart';
+import 'package:ditonton/domain/entities/tv.dart';
+import 'package:ditonton/domain/usecases/get_on_air_tvs.dart';
+import 'package:ditonton/presentation/provider/on_air_tvs_notifier.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import 'on_air_tvs_notifier_test.mocks.dart';
+
+@GenerateMocks([GetOnAirTvs])
+void main() {
+  late MockGetOnAirTvs mockGetOnAirTvs;
+  late OnAirTvsNotifier notifier;
+  late int listenerCallCount;
+
+  setUp(() {
+    listenerCallCount = 0;
+    mockGetOnAirTvs = MockGetOnAirTvs();
+    notifier = OnAirTvsNotifier(getOnAirTvs: mockGetOnAirTvs)
+      ..addListener(() {
+        listenerCallCount++;
+      });
+  });
+
+  final tTv = Tv(
+    backdropPath: '/path.jpg',
+    firstAirDate: 'firstAirDate',
+    genreIds: [1, 2, 3],
+    id: 1,
+    name: 'name',
+    originalName: 'originalName',
+    overview: 'overview',
+    popularity: 1.0,
+    posterPath: '/path.jpg',
+    voteAverage: 1.0,
+    voteCount: 1,
+  );
+
+  final tTvList = <Tv>[tTv];
+
+  test('should change state to loading when usecase is called', () async {
+    // arrange
+    when(mockGetOnAirTvs.execute()).thenAnswer((_) async => Right(tTvList));
+    // act
+    notifier.fetchOnAirTvs();
+    // assert
+    expect(notifier.state, RequestState.Loading);
+    expect(listenerCallCount, 1);
+  });
+
+  test('should change tvs data when data is gotten successfully', () async {
+    // arrange
+    when(mockGetOnAirTvs.execute()).thenAnswer((_) async => Right(tTvList));
+    // act
+    await notifier.fetchOnAirTvs();
+    // assert
+    expect(notifier.state, RequestState.Loaded);
+    expect(notifier.tvs, tTvList);
+    expect(listenerCallCount, 2);
+  });
+
+  test('should return error when data is unsuccessful', () async {
+    // arrange
+    when(mockGetOnAirTvs.execute())
+        .thenAnswer((_) async => Left(ServerFailure('Server Failure')));
+    // act
+    await notifier.fetchOnAirTvs();
+    // assert
+    expect(notifier.state, RequestState.Error);
+    expect(notifier.message, 'Server Failure');
+    expect(listenerCallCount, 2);
+  });
+}
