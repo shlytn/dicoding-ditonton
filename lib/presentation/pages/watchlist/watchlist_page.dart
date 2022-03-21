4f4190c8efbb9765388313b58f331ed41c55550a
Shelly Tansil <shellytansil93@gmail.com>
2022-03-21 20:15:58 +0700

    refactor watchlist movie page

---
+++ lib/presentation/pages/watchlist/watchlist_page.dart
@@ -0,0 +1,45 @@
+import 'package:ditonton/presentation/pages/watchlist/watchlist_movies_page.dart';
+import 'package:ditonton/presentation/pages/watchlist/watchlist_tv_page.dart';
+import 'package:ditonton/presentation/widgets/dropdown_widget.dart';
+import 'package:flutter/material.dart';
+
+class WatchlistPage extends StatefulWidget {
+  static const ROUTE_NAME = '/watchlist';
+
+  @override
+  _WatchlistPageState createState() => _WatchlistPageState();
+}
+
+class _WatchlistPageState extends State<WatchlistPage> with RouteAware {
+  List<String> dropdownItems = ['Tv Series', 'Movie'];
+  String? value = 'Tv Series';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Watchlist'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            DropdownWidget(
+              value: value,
+              items: dropdownItems,
+              onChanged: (value) => setState(() {
+                this.value = value;
+              }),
+            ),
+            Expanded(
+              child: value == dropdownItems[0]
+                  ? WatchlistTvsPage()
+                  : WatchlistMoviesPage(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
