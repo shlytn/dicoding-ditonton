9e1682034efe897e35e65a02332748a1efb14e56
Shelly Tansil <shellytansil93@gmail.com>
2022-03-21 20:14:18 +0700

    add tv card list

---
+++ lib/presentation/widgets/tv_card_list.dart
@@ -0,0 +1,26 @@
+import 'package:ditonton/domain/entities/tv.dart';
+import 'package:ditonton/presentation/pages/tv_detail_page.dart';
+import 'package:ditonton/presentation/widgets/card_list.dart';
+import 'package:flutter/material.dart';
+
+class TvCard extends StatelessWidget {
+  final Tv tv;
+
+  TvCard(this.tv);
+
+  @override
+  Widget build(BuildContext context) {
+    return CardList(
+      title: tv.name,
+      overview: tv.overview,
+      posterPath: tv.posterPath,
+      onTap: () {
+        Navigator.pushNamed(
+          context,
+          TvDetailPage.ROUTE_NAME,
+          arguments: tv.id,
+        );
+      },
+    );
+  }
+}
---
+++ lib/presentation/widgets/tv_list.dart
@@ -0,0 +1,34 @@
+import 'package:ditonton/domain/entities/tv.dart';
+import 'package:ditonton/presentation/pages/tv_detail_page.dart';
+import 'package:ditonton/presentation/widgets/horizontal_card_list.dart';
+import 'package:flutter/material.dart';
+
+class TvList extends StatelessWidget {
+  final List<Tv> tvs;
+
+  TvList(this.tvs);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 200,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          final tv = tvs[index];
+          return HorizontalCardList(
+            posterPath: tv.posterPath,
+            onTap: () {
+              Navigator.pushNamed(
+                context,
+                TvDetailPage.ROUTE_NAME,
+                arguments: tv.id,
+              );
+            },
+          );
+        },
+        itemCount: tvs.length,
+      ),
+    );
+  }
+}
