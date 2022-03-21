7e1abe45939af4ef1ad289bb0b5aad1e2da37ea2
Shelly Tansil <shellytansil93@gmail.com>
2022-03-21 19:48:53 +0700

    add tv and season models

---
+++ lib/data/models/tv_detail_model.dart
@@ -0,0 +1,122 @@
+import 'package:ditonton/data/models/genre_model.dart';
+import 'package:ditonton/data/models/season_model.dart';
+import 'package:ditonton/domain/entities/tv_detail.dart';
+import 'package:equatable/equatable.dart';
+
+class TvDetailModel extends Equatable {
+  TvDetailModel({
+    required this.adult,
+    required this.backdropPath,
+    required this.episodeRunTime,
+    required this.firstAirDate,
+    required this.genres,
+    required this.id,
+    required this.lastAirDate,
+    required this.name,
+    required this.numberOfEpisodes,
+    required this.numberOfSeasons,
+    required this.originalName,
+    required this.overview,
+    required this.posterPath,
+    required this.seasons,
+    required this.voteAverage,
+    required this.voteCount,
+  });
+
+  final bool adult;
+  final String? backdropPath;
+  final List<int> episodeRunTime;
+  final String firstAirDate;
+  final List<GenreModel> genres;
+  final int id;
+  final String lastAirDate;
+  final String name;
+  final int numberOfEpisodes;
+  final int numberOfSeasons;
+  final String originalName;
+  final String overview;
+  final String? posterPath;
+  final List<SeasonModel> seasons;
+  final double voteAverage;
+  final int voteCount;
+
+  factory TvDetailModel.fromJson(Map<String, dynamic> json) => TvDetailModel(
+        adult: json["adult"],
+        backdropPath: json["backdrop_path"],
+        episodeRunTime: List<int>.from(json["episode_run_time"].map((x) => x)),
+        firstAirDate: json["first_air_date"],
+        genres: List<GenreModel>.from(
+            json["genres"].map((x) => GenreModel.fromJson(x))),
+        id: json["id"],
+        lastAirDate: json["last_air_date"],
+        name: json["name"],
+        numberOfEpisodes: json["number_of_episodes"],
+        numberOfSeasons: json["number_of_seasons"],
+        originalName: json["original_name"],
+        overview: json["overview"],
+        posterPath: json["poster_path"],
+        seasons: List<SeasonModel>.from(
+            json["seasons"].map((x) => SeasonModel.fromJson(x))),
+        voteAverage: json["vote_average"].toDouble(),
+        voteCount: json["vote_count"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "adult": adult,
+        "backdrop_path": backdropPath,
+        "episode_run_time": List<dynamic>.from(episodeRunTime.map((x) => x)),
+        "first_air_date": firstAirDate,
+        "genres": List<dynamic>.from(genres.map((x) => x.toJson())),
+        "id": id,
+        "last_air_date": lastAirDate,
+        "name": name,
+        "number_of_episodes": numberOfEpisodes,
+        "number_of_seasons": numberOfSeasons,
+        "original_name": originalName,
+        "overview": overview,
+        "poster_path": posterPath,
+        "seasons": List<dynamic>.from(seasons.map((x) => x.toJson())),
+        "vote_average": voteAverage,
+        "vote_count": voteCount,
+      };
+
+  TvDetail toEntity() {
+    return TvDetail(
+        adult: this.adult,
+        backdropPath: this.backdropPath,
+        episodeRunTime: this.episodeRunTime,
+        firstAirDate: this.firstAirDate,
+        genres: this.genres.map((genre) => genre.toEntity()).toList(),
+        id: this.id,
+        lastAirDate: this.lastAirDate,
+        name: this.name,
+        numberOfEpisodes: this.numberOfEpisodes,
+        numberOfSeasons: this.numberOfSeasons,
+        originalName: this.originalName,
+        overview: this.overview,
+        posterPath: this.posterPath,
+        seasons: this.seasons.map((season) => season.toEntity()).toList(),
+        voteAverage: this.voteAverage,
+        voteCount: this.voteCount);
+  }
+
+  @override
+  List<Object?> get props => [
+        adult,
+        backdropPath,
+        episodeRunTime,
+        firstAirDate,
+        genres,
+        id,
+        lastAirDate,
+        name,
+        numberOfEpisodes,
+        numberOfSeasons,
+        originalName,
+        overview,
+        posterPath,
+        seasons,
+        voteAverage,
+        voteCount,
+      ];
+}
