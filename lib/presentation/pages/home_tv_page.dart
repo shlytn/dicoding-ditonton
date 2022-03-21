82a4bf30bb7411dcdeda113407d96145fded6673
Shelly Tansil <shellytansil93@gmail.com>
2022-03-21 20:15:29 +0700

    add tv pages

---
+++ lib/presentation/pages/home_tv_page.dart
@@ -0,0 +1,96 @@
+import 'package:ditonton/common/state_enum.dart';
+import 'package:ditonton/presentation/pages/on_air_tvs_page.dart';
+import 'package:ditonton/presentation/pages/popular_tvs_page.dart';
+import 'package:ditonton/presentation/pages/top_rated_tvs_page.dart';
+import 'package:ditonton/presentation/provider/tv_list_notifier.dart';
+import 'package:ditonton/presentation/widgets/home_scaffold.dart';
+import 'package:ditonton/presentation/widgets/sub_heading.dart';
+import 'package:ditonton/presentation/widgets/tv_list.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class HomeTvPage extends StatefulWidget {
+  static const ROUTE_NAME = '/home-tv';
+
+  @override
+  State<HomeTvPage> createState() => _HomeTvPageState();
+}
+
+class _HomeTvPageState extends State<HomeTvPage> {
+  @override
+  void initState() {
+    super.initState();
+    Future.microtask(() => Provider.of<TvListNotifier>(context, listen: false)
+      ..fetchOnAirTvs()
+      ..fetchPopularTvs()
+      ..fetchTopRatedTvs());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return HomeScaffold(
+      route: HomeTvPage.ROUTE_NAME,
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SubHeading(
+                title: 'Tv Airing',
+                onTap: () =>
+                    Navigator.pushNamed(context, OnAirTvsPage.ROUTE_NAME),
+              ),
+              Consumer<TvListNotifier>(builder: (context, data, child) {
+                final state = data.onAirState;
+                if (state == RequestState.Loading) {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                } else if (state == RequestState.Loaded) {
+                  return TvList(data.onAirTvs);
+                } else {
+                  return Text('Failed');
+                }
+              }),
+              SubHeading(
+                title: 'Popular',
+                onTap: () =>
+                    Navigator.pushNamed(context, PopularTvsPage.ROUTE_NAME),
+              ),
+              Consumer<TvListNotifier>(builder: (context, data, child) {
+                final state = data.popularTvsState;
+                if (state == RequestState.Loading) {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                } else if (state == RequestState.Loaded) {
+                  return TvList(data.popularTvs);
+                } else {
+                  return Text('Failed');
+                }
+              }),
+              SubHeading(
+                title: 'Top Rated',
+                onTap: () =>
+                    Navigator.pushNamed(context, TopRatedTvsPage.ROUTE_NAME),
+              ),
+              Consumer<TvListNotifier>(builder: (context, data, child) {
+                final state = data.topRatedTvsState;
+                if (state == RequestState.Loading) {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                } else if (state == RequestState.Loaded) {
+                  return TvList(data.topRatedTvs);
+                } else {
+                  return Text('Failed');
+                }
+              }),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
