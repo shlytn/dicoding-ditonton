ba9c8ff869a6666ae2134649e080ae589863c7b7
Shelly Tansil <shellytansil93@gmail.com>
2022-03-22 18:01:55 +0700

    export code

--- search/lib/search.dart
+++ search/lib/search.dart
@@ -1,2 +1,11 @@
 library search;
 
+// movie
+export 'domain/usecases/search_movies.dart';
+export 'presentation/provider/movie_search_notifier.dart';
+
+// tv
+export 'domain/usecases/search_tvs.dart';
+export 'presentation/provider/tv_search_notifier.dart';
+
+export 'presentation/pages/search_page.dart';
