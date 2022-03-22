1b42e24d32468497ac4180cbad29c9c9577e3241
Shelly Tansil <shellytansil93@gmail.com>
2022-03-23 02:07:58 +0700

    export code

--- core/lib/core.dart
+++ core/lib/core.dart
@@ -1,5 +1,6 @@
 library core;
 
+// utils
 export 'styles/colors.dart';
 export 'styles/text_styles.dart';
 export 'utils/constants.dart';
@@ -7,3 +8,13 @@ export 'utils/exception.dart';
 export 'utils/failure.dart';
 export 'utils/state_enum.dart';
 export 'utils/utils.dart';
+
+// entities
+export 'domain/entities/movie.dart';
+export 'domain/entities/movie_detail.dart';
+export 'domain/entities/tv.dart';
+export 'domain/entities/tv_detail.dart';
+
+// repository
+export 'domain/repositories/movie_repository.dart';
+export 'domain/repositories/tv_repository.dart';
--- watchlist/lib/watchlist.dart
+++ watchlist/lib/watchlist.dart
@@ -1,3 +1,19 @@
 library watchlist;
 
+// usecases
+export 'domain/usecases/get_watchlist_movies.dart';
+export 'domain/usecases/get_watchlist_status.dart';
+export 'domain/usecases/save_watchlist.dart';
+export 'domain/usecases/remove_watchlist.dart';
 
+export 'domain/usecases/get_watchlist_tvs.dart';
+export 'domain/usecases/get_watchlist_tv_status.dart';
+export 'domain/usecases/save_watchlist_tv.dart';
+export 'domain/usecases/remove_watchlist_tv.dart';
+
+// provider
+export 'presentation/provider/watchlist_movie_notifier.dart';
+export 'presentation/provider/watchlist_tv_notifier.dart';
+
+// pages
+export 'presentation/pages/watchlist_page.dart';
