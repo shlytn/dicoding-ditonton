c5c6bcc1e303ad5edd4c3fe60bc970ca98e6366f
Shelly Tansil <shellytansil93@gmail.com>
2022-03-27 14:29:33 +0700

    export usecases code

--- core/lib/core.dart
+++ core/lib/core.dart
@@ -18,3 +18,16 @@ export 'domain/entities/tv_detail.dart';
 // repository
 export 'domain/repositories/movie_repository.dart';
 export 'domain/repositories/tv_repository.dart';
+
+// usecases
+export 'domain/usecases/get_movie_detail.dart';
+export 'domain/usecases/get_movie_recommendations.dart';
+export 'domain/usecases/get_now_playing_movies.dart';
+export 'domain/usecases/get_popular_movies.dart';
+export 'domain/usecases/get_top_rated_movies.dart';
+
+export 'domain/usecases/get_tv_detail.dart';
+export 'domain/usecases/get_tv_recommendations.dart';
+export 'domain/usecases/get_on_air_tvs.dart';
+export 'domain/usecases/get_popular_tvs.dart';
+export 'domain/usecases/get_top_rated_tvs.dart';
